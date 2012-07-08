72e1fd16c17390998b4ce55ef6fbfd98bd24ff51
Michael P. Daugherty <mike@mpdaugherty.com>
2012-07-08 11:14:03 +0800

    Adding a view method to demonstrate getting a parameter from the requested URL.

--- demo/urls.py
+++ demo/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'demo.views.home', name='home'),
     url(r'^pretty_quote', 'demo.views.home_with_template', name='pretty_quote'),
+    url(r'^quote/(?P<quote_index>\d+)$', 'demo.views.specific_quote', name='specific_quote'),
 )
--- demo/views.py
+++ demo/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
-from daily_quote import get_quote
+from random_quote import quotes, get_quote
 
 def home(req):
     return HttpResponse(get_quote())
@@ -11,3 +11,12 @@ def home_with_template(req):
         'quote': get_quote(),
     }
     return render_to_response('home.django', context)
+
+def specific_quote(req, quote_index):
+    quote_index = int(quote_index)
+
+    try:
+        quote = quotes[quote_index]
+    except IndexError:
+        quote = 'This quote does not exist'
+    return render_to_response('home.django', { 'quote': quote })
